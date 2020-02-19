b0f9f7f855f7dff6df53538004ab2252d1a73f4b
StasVitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-02-19 15:33:46 -0800

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,26 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(arr instanceof Array === false) {
+        throw new Error;
+    }
+    let newArr = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === '--discard-next' || arr[i-1] === '--discard-next') {
+
+        } else if(arr[i] === "--discard-prev") {
+            if( newArr.length !== 0) {
+                newArr.length = newArr.length -1;
+            }
+        } else if(arr[i] === "--double-next") {
+            if(arr[i+1] !== undefined) {
+                newArr.push(arr[i+1])
+            }
+        } else if(arr[i] === "--double-prev") {
+            if( arr[i-1] !== undefined) {
+                newArr.push(arr[i-1]);
+            }
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
