2d17f5e342bba6b9e6dfeb7cfaa54a159bb34bc4
StasVitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-02-19 17:41:20 -0800

    feat: add simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,85 @@
 const chainMaker = {
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return 0;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return new Node(value);
   },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+  removeLink() {
+    throw new Error;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return "";
   }
 };
 
 module.exports = chainMaker;
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    addLink(value = "") {
+        if (this.next === null) {
+            this.next = new Node(value)
+        } else {
+          this.next.addLink(value);
+        }
+        return this;
+    }
+    removeLink(position) {
+        if (typeof position !== 'number' || position <= 0) {
+            throw new Error;
+        }
+        let count = 1;
+        let table = this;
+        while (table !== null) {
+          if(position === count) {
+            table.value = table.next.value;
+            table.next = table.next.next;
+            return this;
+          }
+          count++;
+          table = table.next;
+        }
+        throw new Error;
+    }
+    reverseChain() {
+      let newList = new Node(this.value);
+      let table = this.next;
+      while(table !== null) {
+          newList = this.prepend(newList, table.value);
+          table = table.next;
+      }
+      return newList;
+    }
+    prepend(list, value) {
+      let newList = new Node;
+      newList.value = value;
+      newList.next = list;
+      return newList;
+    }
+    getLength() {
+      let length = 0;
+      let table = this;
+      while(table !== null) {
+        length++;
+        table = table.next;
+      }
+      return length;
+    }
+    finishChain() {
+      let table = this;
+      let result = [];
+      while(table !== null) {
+        result.push( "( " + String(table.value) + " )");
+        table = table.next;
+      }
+      return result.join("~~");
+    }
+}
